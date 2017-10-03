4512c38b423468987c3b83f6a72a274de9337217
Lancy Goyal <lancy@ignivasolutions.com>
2017-10-03 19:13:03 +0530

    Initial Commit

---
+++ server/main.js
@@ -0,0 +1,47 @@
+import { Meteor } from 'meteor/meteor';
+import http from 'http';
+import socket_io from 'socket.io';
+
+const PORT = 3003;
+
+// Client-side config
+WebAppInternals.addStaticJs(`
+  window.socketPort = ${PORT};
+`);
+
+Meteor.startup(() => {
+    // Server
+    const server = http.createServer();
+    const io = socket_io(server);
+
+    let counter = 0;
+
+    // New client
+    io.on('connection', function(socket) {
+
+        console.log('new socket client');
+
+        socket.emit('counter', counter);
+
+        // Counter update from client
+        socket.on('counter', (value) => {
+            console.log(`counter changed on client: ${value}`);
+            if (counter !== value) {
+                io.emit('counter', counter = value);
+            }
+        });
+
+        socket.on('disconnect', function() {
+            console.log('socket disconnect');
+        });
+        
+    });
+
+    // Start server
+    try {
+        server.listen(PORT);
+    } catch (e) {
+        console.error(e);
+    }
+
+});
